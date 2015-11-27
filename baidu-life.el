3a68904d304af7f998366eb9c59ab05d68d825b1
lujun9972 <lujun9972@gmail.com>
2015-11-27 19:22:41 +0800

    add comment

--- baidu-life.el
+++ baidu-life.el
@@ -1,3 +1,39 @@
+;;; baidu-life.el --- Use baidu api to do some interesting things
+
+;; Copyright (C) 2004-2015 Free Software Foundation, Inc.
+
+;; Author: DarkSun <lujun9972@gmail.com>
+;; Keywords: lisp, baidu
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Source code
+;;
+;; baidu-life's code can be found here:
+;;   http://github.com/lujun9972/el-baidu-life
+
+;;; Commentary:
+
+;; There are some interesting apis in baidu api store.
+;; so I write this package to encapsulates some apis and makes them easy
+;; to use in emacs.
+
+
+;;; Code:
+
 (require 'cl)
 (require 'url)
 (defgroup baidu-life nil
